ba2bda61e06dc48f758aa9e3d454f7e5b39fa64d
charlietfl <charlietfl@gmail.com>
2013-07-07 19:43:22 -0400

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: charl_000
+ * Date: 7/7/13
+ * Time: 6:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+StackMob.init({
+    publicKey: "42e9c819-d466-4cbb-a43d-367d963c5264",
+    apiVersion: 0
+});
